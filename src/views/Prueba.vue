6e1bc2636ae902f1c6da7c6dee8d2fc25ed71ced
MaiteVazquez8 <maite.lu.vazquez08@gmail.com>
2026-09-16 10:47:50 -0300

    clase

---
+++ src/views/Prueba.vue
@@ -0,0 +1,23 @@
+<template>
+    <q-layout view="hhh lpr fff">
+        <q-header>
+            <q-toolbar class="barra de navegación">
+                <q-toolbar class="marca">
+                    KodiGo
+                </q-toolbar>
+            </q-toolbar>
+        </q-header>
+        <q-page-container>
+            <nav>
+                <q-btn flat no-caps label="Inicio" to="/"></q-btn>
+            </nav>
+        </q-page-container>
+    </q-layout>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
