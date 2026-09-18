9ccc67ad52f4a9fb0c935ecaa935c10158e3d0c7
MaiteVazquez8 <maite.lu.vazquez08@gmail.com>
2026-09-18 08:59:29 -0300

    clase

--- src/views/Prueba.vue
+++ src/views/Prueba.vue
@@ -16,6 +16,51 @@
 </template>
 
 <script setup>
+    import { ref } from 'vue'
+    import {supabase} from '@/supabase'
+    
+    const correo=ref('')
+    const password=ref('')
+    const repetirPassword=ref('')
+    const cargando=ref(false)
+    const mensaje=ref('')
+    const registroExitoso=ref(false)
+
+    async function registrarUsuario() {
+        mensaje.value = ''
+        registroExitoso.value = false
+        if (password.value !== repetirPassword.value) {
+            mensaje.value = 'Las contraseñas no coinciden'
+            return
+        }
+        if (password.value.length < 6) {
+            mensaje.value = 'La contraseña debe tener al menos 6 caracteres'
+            return
+        }
+
+        try{
+            cargando.value = true
+            const { error } = await supabase.auth.signUp({
+                email: correo.value,
+                password: password.value,
+            })
+            cargando.value = false
+            if (error) {
+                throw error
+            } else {
+                registroExitoso.value = true
+                mensaje.value = 'Registro realizado. Revisa tu Email.'
+
+                correo.value = ''
+                password.value = ''
+                repetirPassword.value = ''
+            }
+        } catch (error) {
+            mensaje.value=error.message
+        } finally {
+            cargando.value = false
+        }
+    }
 </script>
 
 <style scoped>
